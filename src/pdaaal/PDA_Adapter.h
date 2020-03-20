ca2423f95af7cfcd2030eba47dbf3c2a03729993
Morten Konggaard Schou <mschou15@student.aau.dk>
2020-03-20 15:39:31 +0100

    Forgot to add license

--- src/pdaaal/PDA_Adapter.h
+++ src/pdaaal/PDA_Adapter.h
@@ -1,6 +1,28 @@
-//
-// Created by Morten on 17-03-2020.
-//
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ *  Copyright Morten K. Schou
+ */
+
+/*
+ * File:   Weight.h
+ * Author: Morten K. Schou <morten@h-schou.dk>
+ *
+ * Created on 17-03-2020.
+ */
 
 #ifndef PDAAAL_PDA_ADAPTER_H
 #define PDAAAL_PDA_ADAPTER_H
