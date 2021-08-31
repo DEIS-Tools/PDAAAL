3b348c5152bce0be8a4a0fe837000b55208a403e
Morten Schou <mksc@cs.aau.dk>
2021-08-31 10:27:26 +0200

    Add 'indirect' option to PAutomatonParser

--- src/pdaaal-bin/parsing/PAutomatonParser.h
+++ src/pdaaal-bin/parsing/PAutomatonParser.h
@@ -65,14 +65,14 @@ namespace pdaaal {
     struct p_automaton_file : pegtl::must<pegtl::pad<p_automaton_expr<p_automaton_state>, ignored<comment>>, pegtl::eof> {};
 
     // The State object that gets passed around by the parser is a builder that constructs the PAutomaton.
-    template<typename label_t, typename W, typename state_t, bool skip_state_mapping>
+    template<typename label_t, typename W, typename state_t, bool skip_state_mapping, bool indirect>
     class PAutomatonBuilder {
     public:
         explicit PAutomatonBuilder(TypedPDA<label_t,W,fut::type::vector,state_t,skip_state_mapping>& pda,
                                    const std::function<state_t(const std::string&)>& state_mapping)
         : _pda(pda), _state_mapping(state_mapping),
           _label_mapping([&pda](const std::string& label) -> uint32_t { return pda.insert_label(label); }) {};
-        PAutomaton<W> get_p_automaton() {
+        PAutomaton<W,indirect> get_p_automaton() {
             return _current_p_automaton.value();
         }
         bool add_state(const std::string& state_name) {
@@ -95,7 +95,7 @@ namespace pdaaal {
         void finish_atom() {
             std::sort(_states.begin(), _states.end()); // Sort and remove duplicates. TODO: Consider using unordered_set instead...
             _states.erase(std::unique(_states.begin(), _states.end()), _states.end());
-            PAutomaton<W> new_p_automaton(_pda, _current_nfa, _states);
+            PAutomaton<W,indirect> new_p_automaton(_pda, _current_nfa, _states);
             if (_current_p_automaton) {
                 _current_p_automaton.value().or_extend(std::move(new_p_automaton));
             } else {
@@ -106,16 +106,21 @@ namespace pdaaal {
     private:
         std::vector<size_t> _states;
         NFA<uint32_t> _current_nfa;
-        std::optional<PAutomaton<W>> _current_p_automaton;
+        std::optional<PAutomaton<W,indirect>> _current_p_automaton;
 
         const TypedPDA<label_t,W,fut::type::vector,state_t,skip_state_mapping>& _pda;
         const std::function<state_t(const std::string&)>& _state_mapping;
         std::function<uint32_t(const std::string&)> _label_mapping;
     };
+    template<bool indirect, typename label_t, typename W, typename state_t, bool skip_state_mapping>
+    inline auto make_PAutomatonBuilder(TypedPDA<label_t,W,fut::type::vector,state_t,skip_state_mapping>& pda,
+                                       const std::function<state_t(const std::string&)>& state_mapping) {
+        return PAutomatonBuilder<label_t,W,state_t,skip_state_mapping,indirect>(pda, state_mapping);
+    }
     // CTAD guide
     template<typename label_t, typename W, typename state_t, bool skip_state_mapping>
     PAutomatonBuilder(TypedPDA<label_t,W,fut::type::vector,state_t,skip_state_mapping>& pda,
-                      const std::function<state_t(const std::string&)>& state_mapping) -> PAutomatonBuilder<label_t,W,state_t,skip_state_mapping>;
+                      const std::function<state_t(const std::string&)>& state_mapping) -> PAutomatonBuilder<label_t,W,state_t,skip_state_mapping,true>;
 
     // Definition of the actions applied by the parser to the builder state object.
     // Combine NFA action, unescape action, and a new action that adds states.
@@ -123,14 +128,14 @@ namespace pdaaal {
     template<> struct p_automaton_build_action< pegtl::utf8::range< 0x20, 0x10FFFF > > : pegtl::unescape::append_all {};
     template<> struct p_automaton_build_action< escaped_c > : pegtl::unescape::unescape_c< escaped_c, '"', '\\' > {};
     template<> struct p_automaton_build_action<p_automaton_state_1> {
-        template<typename ActionInput, typename T, typename W, typename S, bool ssm> static void apply(const ActionInput& in, PAutomatonBuilder<T,W,S,ssm>& v) {
+        template<typename ActionInput, typename T, typename W, typename S, bool ssm, bool i> static void apply(const ActionInput& in, PAutomatonBuilder<T,W,S,ssm,i>& v) {
             if (!v.add_state(in.string())) {
                 throw pegtl::parse_error("state is not found in the PDA.", in);
             }
         }
     };
     template<typename State> struct p_automaton_build_action<p_automaton_atom<State>> {
-        template<typename T, typename W, typename S, bool ssm> static void apply0(PAutomatonBuilder<T,W,S,ssm>& v) {
+        template<typename T, typename W, typename S, bool ssm, bool i> static void apply0(PAutomatonBuilder<T,W,S,ssm,i>& v) {
             v.finish_atom();
         }
     };
@@ -138,30 +143,30 @@ namespace pdaaal {
     // Final parser class.
     class PAutomatonParser {
     public:
-        template <typename pda_t>
+        template <bool indirect = true, typename pda_t>
         static auto parse_file(const std::string& file, pda_t& pda) {
             std::filesystem::path file_path(file);
             pegtl::file_input in(file_path);
-            return parse(in, pda);
+            return parse<indirect>(in, pda);
         }
-        template <typename pda_t>
+        template <bool indirect = true, typename pda_t>
         static auto parse_string(const std::string& content, pda_t& pda) {
             pegtl::memory_input in(content, "");
-            return parse(in, pda);
+            return parse<indirect>(in, pda);
         }
 
     private:
-        template <typename Input, typename W>
-        static PAutomaton<W> parse(Input& in, TypedPDA<std::string,W,fut::type::vector, std::string>& pda) {
-            return parse<std::string>(in, pda, [](const std::string& s){ return s; });
+        template <bool indirect, typename Input, typename W>
+        static PAutomaton<W,indirect> parse(Input& in, TypedPDA<std::string,W,fut::type::vector, std::string>& pda) {
+            return parse<indirect, std::string>(in, pda, [](const std::string& s){ return s; });
         }
-        template <typename Input, typename W, bool ssm>
-        static PAutomaton<W> parse(Input& in, TypedPDA<std::string,W,fut::type::vector, size_t, ssm>& pda) {
-            return parse<size_t>(in, pda, [](const std::string& s) -> size_t { return std::stoul(s); });
+        template <bool indirect, typename Input, typename W, bool ssm>
+        static PAutomaton<W,indirect> parse(Input& in, TypedPDA<std::string,W,fut::type::vector, size_t, ssm>& pda) {
+            return parse<indirect, size_t>(in, pda, [](const std::string& s) -> size_t { return std::stoul(s); });
         }
-        template <typename state_t, typename Input, typename pda_t>
+        template <bool indirect, typename state_t, typename Input, typename pda_t>
         static auto parse(Input& in, pda_t& pda, const std::function<state_t(const std::string&)>& state_mapping) {
-            PAutomatonBuilder p_automaton_builder(pda, state_mapping);
+            auto p_automaton_builder = make_PAutomatonBuilder<indirect>(pda, state_mapping);
             try {
                 pegtl::parse<p_automaton_file,p_automaton_build_action>(in, p_automaton_builder);
             } catch (const pegtl::parse_error& e) {
