7e42b7552b875fa93ef29782a1985a69870ee493
Morten Schou <mksc@cs.aau.dk>
2022-03-04 12:32:25 +0100

    Small fixes

--- src/include/pdaaal/utils/absl_hash.h
+++ src/include/pdaaal/utils/absl_hash.h
@@ -30,9 +30,13 @@
 // All code that needs absl::hash should include this file instead of <absl/hash/hash.h>
 // This allows us to compile with -Wpedantic without being spammed by warnings from Abseil.
 // In the case of Abseil, these warnings are false positives, and the Abseil team has decided to not support -Wpedantic.
+#ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
 #include <absl/hash/hash.h>
 #pragma GCC diagnostic pop
+#else
+#include <absl/hash/hash.h>
+#endif
 
 #endif //PDAAAL_ABSL_HASH_H
